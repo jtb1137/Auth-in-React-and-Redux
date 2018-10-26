ae0df6680901b20534b5bd93d0f5915df7e7e1d5
jtb1137 <jtb1137@gmail.com>
2018-10-26 14:52:37 -0400

    add nodemon, create basic server, implement morgan and body-parser middleware

---
+++ server/index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const http = require("http");
+const bodyParser = require("body-parser");
+const morgan = require("morgan");
+const app = express();
+
+// App Setup
+app.use(morgan("combined")); // Middleware - Logging Framework
+app.use(bodyParser.json({ type: "*/*" })); // Middleware - Parse Request to JSON
+
+// Server Setup
+const port = process.env.PORT || 3000;
+const server = http.createServer(app);
+server.listen(port);
+console.log("Server listening on:", port);
