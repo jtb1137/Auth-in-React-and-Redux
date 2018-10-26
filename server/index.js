82ef67722d84b9bfd486d48ab8be564aa4bbce94
jtb1137 <jtb1137@gmail.com>
2018-10-26 15:00:33 -0400

    setup router

--- server/index.js
+++ server/index.js
@@ -3,10 +3,12 @@ const http = require("http");
 const bodyParser = require("body-parser");
 const morgan = require("morgan");
 const app = express();
+const router = require("./router");
 
 // App Setup
 app.use(morgan("combined")); // Middleware - Logging Framework
 app.use(bodyParser.json({ type: "*/*" })); // Middleware - Parse Request to JSON
+router(app);
 
 // Server Setup
 const port = process.env.PORT || 3000;
