df5ed3f4940a0043afa39bb0cebed1f0195795a9
jtb1137 <jtb1137@gmail.com>
2018-10-29 13:29:35 -0400

    fix signin

--- server/router.js
+++ server/router.js
@@ -4,12 +4,12 @@ const passport = require("passport");
 
 const requireAuth = passport.authenticate("jwt", { session: false });
 // By default, passport wants to create a cookie-based session
-const requireSignin = passport.authenticate("local", { sessions: false });
+const requireSignin = passport.authenticate("local", { session: false });
 
 module.exports = function(app) {
   app.get("/", requireAuth, function(req, res) {
     res.send({ hi: "there" });
   });
-  app.post("/signin", Authentication.signin);
+  app.post("/signin", requireSignin, Authentication.signin);
   app.post("/signup", Authentication.signup);
 };
